46918cde225e90236d374087822553a92cff1a54
chandan <chandan577516@gmail.com>
2024-10-03 02:52:51 +0530

    first commit

---
+++ src/components/Credit.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div class="credit">
+      <p>
+        Coded by <span className="textred">chandan gupta</span>
+      </p>
+    </div>
+  );
+}
